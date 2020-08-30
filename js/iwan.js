b37de8e83fcd030bcd9aabb8dbac857149362343
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-08-31 00:36:12 +0800

    Add files via upload

---
+++ js/iwan.js
@@ -0,0 +1,84 @@
+function play() {
+       document.getElementById('img1').style.display = "none";
+       document.getElementById('vid1').style.display = "flex";
+       document.getElementsByTagName('video')[0].play();
+       setInterval(skin1,7200);
+       }
+       
+       function skin1() {
+       document.getElementById('vid1').style.display = "none";
+       document.getElementById('skin1').style.display = "block";
+       }
+       
+       
+       
+       function play2() {
+       document.getElementById('img2').style.display = "none";
+       document.getElementById('vid2').style.display = "flex";
+       document.getElementsByTagName('video')[1].play();
+       setInterval(skin2,7200);
+       }
+       
+       function skin2() {
+       document.getElementById('vid2').style.display = "none";
+       document.getElementById('skin2').style.display = "block";
+       }
+      
+      
+      
+      
+      
+      
+      function play3() {
+       document.getElementById('img3').style.display = "none";
+       document.getElementById('vid3').style.display = "flex";
+       document.getElementsByTagName('video')[2].play();
+       setInterval(skin3,7200);
+       }
+       
+       function skin3() {
+       document.getElementById('vid3').style.display = "none";
+       document.getElementById('skin3').style.display = "block";
+       }
+       
+       
+       
+       function play4() {
+       document.getElementById('img4').style.display = "none";
+       document.getElementById('vid4').style.display = "flex";
+       document.getElementsByTagName('video')[3].play();
+       setInterval(skin4,7200);
+       }
+       
+       function skin4() {
+       document.getElementById('vid4').style.display = "none";
+       document.getElementById('skin4').style.display = "block";
+       }
+       
+       
+       
+       function play5() {
+       document.getElementById('img5').style.display = "none";
+       document.getElementById('vid5').style.display = "flex";
+       document.getElementsByTagName('video')[4].play();
+       setInterval(skin5,7200);
+       }
+       
+       function skin5() {
+       document.getElementById('vid5').style.display = "none";
+       document.getElementById('skin5').style.display = "block";
+       }
+       
+       
+       
+       function play6() {
+       document.getElementById('img6').style.display = "none";
+       document.getElementById('vid6').style.display = "flex";
+       document.getElementsByTagName('video')[5].play();
+       setInterval(skin6,7200);
+       }
+       
+       function skin6() {
+       document.getElementById('vid6').style.display = "none";
+       document.getElementById('skin6').style.display = "block";
+       }
